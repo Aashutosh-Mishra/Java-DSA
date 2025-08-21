7e3c6fb5998e7a734db9f3e6c6e7f4f661069566
Aashutosh Mishra <aashu.utir@gmail.com>
2025-08-22 01:39:31 +0530

    literals

---
+++ basics/litreals.java
@@ -0,0 +1,16 @@
+public class litreals {
+    public static void main(String args[])
+    {
+        byte age2=35;
+        short age3=100;
+        int age=47;
+        long l2=123456L;
+        float f1=10.5f;
+        double d1=25.5;
+        boolean a=true;
+        char cha='A';
+        String str="sdfa";
+        System.out.println(age);
+    }
+    
+}
